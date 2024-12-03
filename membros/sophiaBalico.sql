34df9a038a3d10c6a0cf768c991f96cee09e4580
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-12-03 07:58:16 -0300

    feat: criação das tabelas e organização de corpo de banco completo

--- membros/sophiaBalico.sql
+++ membros/sophiaBalico.sql
@@ -72,31 +72,7 @@ CREATE TABLE entrevistas (
     FOREIGN KEY (id_professor) REFERENCES professores(id_professor)
 );
 
--- insert professores
 
 INSERT INTO professores (nome) VALUES
 ('Eva'),
 ('Paula Jubilut');
-
--- insert carreiras
-
-INSERT INTO carreiras (profissao, descricao, url_imagem_carreira) VALUES
-('Biólogo', 'Profissional que estuda os seres vivos', NULL),
-('Biomédico', 'Profissional que estuda as doenças e os agentes causadores', NULL),
-('Zoólogo', 'Profissional que estuda os animais', NULL),
-('Ictiologia', 'Profissional que estuda os peixes', NULL),
-('Etologia', 'Profissional que estuda o comportamento animal', NULL),
-('Bioética', 'Profissional que estuda a ética na biologia', NULL),
-('Bioinformática', 'Profissional que estuda a informática na biologia', NULL),
-('Ecoturismo', 'Profissional que estuda o turismo ecológico', NULL),
-('Biologia Molecular', 'Profissional que estuda a biologia molecular', NULL),
-('Análises Clínicas', 'Profissional que faz análises clínicas', NULL);
-
--- insert usuarios
-
-INSERT INTO usuarios (nome, email) VALUES
-('João Silva', 'joao.silva@gmail.com'),
-('Maria Oliveira', 'maria.oliveira@gmail.com'),
-('José Santos', 'jose.santos@gmail.com'),
-('Ana Pereira', 'ana.pereira@gmail.com'),
-('Pedro Carvalho', 'pedro.carvalho@gmail.com');
