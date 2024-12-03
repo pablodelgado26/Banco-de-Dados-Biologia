2808d967a6805bfb2553653836cb58ef112497f5
Nickcau1203 <nicolecau.tds2.senai@gmail.com>
2024-12-03 08:02:38 -0300

    Noticias e simulados parte Nicole

---
+++ membros/nicoleCau.sql
@@ -0,0 +1,23 @@
+ INSERT INTO autor_noticias (nome, email) VALUES
+('Kimberly da Silva Marta Silva', 'kimberly.silva@gmail.com'),
+('Lucas Andrade', 'Lucas.andrade@gmail.com'),
+('Mirella Cordeiro', 'Mirella.cordeiro@gmail.com'),
+('Milena Gurgel Teles Bezerra', 'Milena.gurgel@gmail.com'),
+('Tiago da Gräf', 'Tiago.gräf@gmail.com'),
+(' Laercio Pereira', 'Laercio.pereira@gmail.com'),
+('Ítana Santos', ' Ítana.santos@gmail.com'),
+('Fernando Freitas', 'Fernando.freitas@gmail.com'),
+(' Marcelo Mori', 'Marcelo.mori@gmail.com'),
+('Peter Barry', 'Peter.barry@gmail.com'),
+('G1', 'G1.globo@gmail.com'),
+('Augusto Ruschi', 'Augusto.ruschi@gmail.com'),
+('Aldem Bourscheit ', 'Aldem.bourscheit @gmail.com'),
+('Deborah Gonçalves', ' Deborah.gonçalves@gmail.com'),
+(' Larissa Fortunato', ' Larissa.fortunato@gmail.com');
+
+
+INSERT INTO simulados (titulo, nivel_simulado, url_simulado) VALUES
+('Simulado de Nível Básico', 'Básico', 'https://docs.google.com/forms/d/1pP8lAp_lHbW7zRDzYXZQ2UVOpyxygP1a0foWr4ktK3Y/viewform?edit_requested=true'),
+('Simulado de Nível Intermediário', 'Intermediário', 'https://docs.google.com/forms/d/1OHZGESlB0UDcx47qfK8Brw8jDqdOVBLMYCaTgB6iw38/viewform?edit_requested=true'),
+('Simulado de Nível Avançado', 'Avançado', 'https://docs.google.com/forms/d/1MP7_29-ajhHva62j1S3FCylU8jGLhpTOXoEXROHxTXE/viewform?edit_requested=true');
+('Simulado de Nível Final', 'Final', 'https://docs.google.com/forms/d/1ApVg3jQ5wl_9-KTH3iU6756HIz9-_L6jVOgfQT4BrgA/viewform?edit_requested=true');
